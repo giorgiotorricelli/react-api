b7c797b4fede01b5a679dff5848b0870f0f15f10
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-07 15:22:47 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const apiUrl = 'https://lanciweb.github.io/demo/api/actresses/';
+
 
 function App() {
+  useEffect(() => {
+    fetch(apiUrl)
+    .then(resp => {
+      return resp.json()
+    }).then(json => { //dentro abbiamo il nostro array
+      console.log(json); 
+    })
+
+  }, []) //array dipendenze vuoto perchè deve fare la fetch solo al mount
+
+
   return <button className='btn btn-primary'>click</button>;
 }
 
